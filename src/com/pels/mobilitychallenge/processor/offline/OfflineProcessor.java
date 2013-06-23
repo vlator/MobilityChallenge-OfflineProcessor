538a9adc55584e916cf9bca2cf6d96005237bd67
Chris Pels <caapels@gmail.com>
2013-06-23 10:02:12 +0200

    Initial Commit

---
+++ src/com/pels/mobilitychallenge/processor/offline/OfflineProcessor.java
@@ -0,0 +1,104 @@
+package com.pels.mobilitychallenge.processor.offline;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import com.pels.mobilitychallenge.base.common.CustomFileFilterByExt;
+import com.pels.mobilitychallenge.base.data.FramedReadings;
+import com.pels.mobilitychallenge.base.data.FramesExtractor;
+import com.pels.mobilitychallenge.base.data.Label;
+import com.pels.mobilitychallenge.base.data.Reading;
+import com.pels.mobilitychallenge.base.data.ReadingsLoader;
+
+public class OfflineProcessor {
+
+	private static final String PROCESSED = "processed_";
+	private static final String PROCESSED_OUTPUT_PATH = "/Users/cpels/Documents/workspace/fraunhofer/data/processed/";
+	File inputDir;
+
+	ReadingsLoader rLoader;
+	FramesExtractor sw;
+	int wLen;
+
+	public OfflineProcessor(String inputDirPath, int windowLength) {
+		this.inputDir = new File(inputDirPath);
+		this.wLen = windowLength;
+		if (inputDir.exists() && inputDir.isDirectory()) {
+			sw = new FramesExtractor(windowLength);
+		} else {
+			System.out.println("Input path error.");
+		}
+
+	}
+
+	private void runProcessing() {
+		File[] allInputFiles = inputDir.listFiles(new CustomFileFilterByExt(
+				"csv"));
+		for (File inputFile : allInputFiles) {
+			System.out.println("Processing: " + inputFile.getName());
+			process(inputFile);
+		}
+	}
+
+	private void process(File inputFile) {
+		String filename = inputFile.getName();
+		try {
+			rLoader = new ReadingsLoader(inputFile);
+			for (Label label : Label.values()) {
+				List<Reading> allFrames = rLoader.filterByLabel(label);
+				List<FramedReadings> framed = sw.getFramedReadings(allFrames);
+
+				setLabelAndWriteFrame(label, filename, framed);
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private void setLabelAndWriteFrame(Label label, String filename,
+			List<FramedReadings> framed) {
+		String path = PROCESSED_OUTPUT_PATH + PROCESSED + wLen + "_"
+				+ filename;
+		System.out.println(path);
+		File outputFile = new File(path);
+		BufferedWriter writer;
+		try {
+			if (!outputFile.exists()) {
+				writer = new BufferedWriter(new FileWriter(outputFile, true));
+				writer.write(FramedReadings.HEADER + "\n");
+			} else {
+				writer = new BufferedWriter(new FileWriter(outputFile, true));
+			}
+			for (FramedReadings r : framed) {
+				r.setLabel(label);
+				if (r.toString() == null) {
+					System.out.println("why???");
+				}
+				writer.write(r.toString() + "\n");
+			}
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String inPath = "/Users/cpels/Documents/workspace/fraunhofer/data/raw_input";
+		int wLen = 20;
+		new OfflineProcessor(inPath, wLen).runProcessing();
+
+	}
+
+}
---
+++ src/com/pels/mobilitychallenge/processor/offline/TrainingFileBuilder.java
@@ -0,0 +1,97 @@
+package com.pels.mobilitychallenge.processor.offline;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pels.mobilitychallenge.base.data.FramedReadings;
+
+public class TrainingFileBuilder {
+	private static final String EXT = ".csv";
+	private static final String INPUT_PREFIX = "processed_";
+	private static final String OUTPUT_PREFIX = "training_";
+	private static final Charset ENCODING = StandardCharsets.UTF_8;
+	
+	private File inputDir;
+	private String outputPath;
+	private int windowLength;
+
+	public TrainingFileBuilder(String inputPath, String outputPath, int windowLength) {
+		this.outputPath = outputPath;
+		inputDir = new File(inputPath);
+		this.windowLength = windowLength;
+
+	
+	}
+	
+	public void writeTrainingFile() throws FileNotFoundException{
+		if (inputDir.exists() && inputDir.isDirectory()) {
+			File[] filtered = inputDir.listFiles(new ProcessedFileFilter(windowLength));
+			writeTrainingFile(filtered);
+		}else{
+			throw new FileNotFoundException("Input directory not found!");
+		}
+	}
+
+	private void writeTrainingFile(File[] filtered) {
+		List<String> allLines = new ArrayList<String>();
+		allLines.add(FramedReadings.HEADER);
+		try {
+			for (File f : filtered) {
+				List<String> temp = Files.readAllLines(f.toPath(), ENCODING);
+				temp.remove(0);
+				allLines.addAll(temp);
+			}
+			writeTrainingFile(allLines);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void writeTrainingFile(List<String> allLines) throws IOException {
+		File output = new File(outputPath + OUTPUT_PREFIX + windowLength + EXT);
+		output.getParentFile().mkdirs();
+	    Path path = output.toPath();
+	    Files.write(path, allLines, ENCODING);
+		
+	}
+
+	class ProcessedFileFilter implements FilenameFilter {
+		private String prefix;
+
+		public ProcessedFileFilter(int wlen) {
+			this.prefix = INPUT_PREFIX + wlen;
+		}
+
+		@Override
+		public boolean accept(File dir, String name) {
+			return name.startsWith(prefix);
+		}
+	}
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String inPath = "/Users/cpels/Documents/workspace/fraunhofer/data/processed/";
+		String outPath = "/Users/cpels/Documents/workspace/fraunhofer/data/training/";
+		int wLen = 20;
+		try {
+			new TrainingFileBuilder(inPath, outPath, wLen).writeTrainingFile();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
